fb69ca28157c92a5e22f33dd184a89267c1e87ad
Venkatesh Jagannathan <venkyzealous@gmail.com>
2012-11-29 23:55:38 +0530

    styling, dojo/on code generation

--- Source/main.js
+++ Source/main.js
@@ -16,7 +16,56 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function showForm(id)
-{
-	var forms = ['','','','']
-}
+
+var dww = {
+	showForm : function (id){
+					dww.selectedForm = id;
+					var ids = ['event','publish','subscribe','xhrCall'];
+					for(var i = 0; i < ids.length; i++)
+						if(id != ids[i])
+							document.forms[ids[i]].style.display='none';
+						else
+							document.forms[ids[i]].style.display='inline';
+	},
+	generate : function(){
+
+		if(dww.selectedForm == 'event'){
+			dww.generateEventCode();
+		}
+	},
+	initialize : function(){
+		document.addEventListener('DOMContentLoaded', function () {
+			document.getElementById('option1').addEventListener('click',dww.showForm.bind(undefined,'event'));
+			document.getElementById('option2').addEventListener('click',dww.showForm.bind(undefined,'publish'));
+			document.getElementById('option3').addEventListener('click',dww.showForm.bind(undefined,'subscribe'));
+			document.getElementById('option4').addEventListener('click',dww.showForm.bind(undefined,'xhrCall')); 
+			document.getElementById('generate').addEventListener('click',dww.generate); 
+		});
+	},
+	generateEventCode:function(){
+		var eventName = $('#eventsValue')[0].value;
+		var contextValue = $('#contextValue')[0].value;
+		var argumentValue = $('#argumentValue')[0].value;
+		var customCodeValue = $('#customCodeValue')[0].value;
+		
+		$('#generated')[0].innerText = "require([\"dojo/on\"], function(on){\n\ton("
+										+contextValue
+										+", \""
+										+eventName
+										+"\", function("
+										+argumentValue
+										+"){\n\t\t"
+										+customCodeValue
+										+"\n\n\t});\n});";
+	},
+	selectedForm : ''
+};
+
+dww.initialize();
+
+
+
+
+
+
+
